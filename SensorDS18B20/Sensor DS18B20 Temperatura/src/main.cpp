c4f6ccea630577b525ce2c3eee5cd414fe026015
AMontt <alanrojasa1@gmail.com>
2020-09-16 15:25:53 -0300

    First try Ds18b20 Serial

---
+++ SensorDS18B20/Sensor DS18B20 Temperatura/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire DXensor_Temperature(2); //Pin 4
+
+DallasTemperature temp(&DXensor_Temperature);
+
+
+void setup() {
+  Serial.begin(9600);
+  temp.begin();
+
+}
+
+void loop() {
+
+  temp.requestTemperatures();
+  float temperatura = temp.getTempCByIndex(0);
+
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" °C");
+  delay(100);
+
+}
